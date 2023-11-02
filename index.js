e2a302f28c0a17e5b16c6a0b3fd866715b242b6a
Ruben <amon007>
2023-11-02 20:03:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.use(
     })
   );
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+       next();
+ });
 
 app.use(express.json())
 app.use(fileUpload({}))
