bbe0218dd2f6100f248619b94dca2a4e7c698f27
Ruben <amon007>
2023-11-02 19:18:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const app = express()
 app.use(
     cors({
       origin: '*', 
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true
+      credentials: true,
+      optionSuccessStatus:200,
     })
   );
 
