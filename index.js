978efc3d26c8eff13c586acf6839a817db915ff5
Ruben <amon007>
2023-11-02 20:24:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
     cors(
       {
       origin: '*', 
-      methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
       credentials: true,
       optionSuccessStatus:200,
     }
