290f8aa86c5abf099f4969e16632c1adf65708e9
Ruben <amon007>
2023-11-02 20:08:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ function cors(req, res, next) {
        next();
  }
 
+ app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // allow preflight
+  if (req.method === 'OPTIONS') {
+      res.send(200);
+  } else {
+      next();
+  }
+});
+
 app.use(express.json())
 app.use(fileUpload({}))
 app.use(express.static('static'))
